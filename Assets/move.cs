c87ee6d0311e8f8d57a8331de4ff315803ace417
erlebach <gordon.erlebach@gmail.com>
2019-09-02 15:21:05 -0400

    Initial commit to remote repository

---
+++ Assets/move.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+    float a;
+    float b = 5;
+    Fancy f;
+
+    // Update is called once per frame
+    void Update () {
+        transform.Rotate (new Vector2 (3f, 4f));
+        a = 3;
+        fancy ();
+        f = new Fancy ();
+    }
+
+    void Going () {
+        fancy ();
+        fancy ();
+    }
+
+    void fancy () {
+        a = 5 + b;
+    }
+}
---
+++ Assets/xxx.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class Fancy : MonoBehaviour {
+    float a;
+
+    void gone () {
+        a = 5f;
+    }
+
+    void fancy () { }
+};
