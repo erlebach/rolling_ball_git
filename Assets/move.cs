1325d868555fde56b9eb4c088c8ad2d46d466540
erlebach <gordon.erlebach@gmail.com>
2019-09-10 11:18:02 -0400

    Starting state on Sept.10, 2019

--- Assets/move.cs
+++ Assets/move.cs
@@ -4,23 +4,33 @@ using UnityEngine;
 
 public class move : MonoBehaviour {
     float a;
-    float b = 5;
-    Fancy f;
+    [SerializeField] float speed;
+    Vector3 pos;
+    float x;
+    [SerializeField] float direc;
+    [SerializeField] float xinc;
+
+    private void Start () {
+        x = 0f;
+        direc = -direc;
+    }
 
     // Update is called once per frame
     void Update () {
-        transform.Rotate (new Vector2 (3f, 4f));
-        a = 3;
-        fancy ();
-        f = new Fancy ();
+        horizontalOscillation ();
     }
 
-    void Going () {
-        fancy ();
-        fancy ();
+    void horizontalOscillation () {
+        // Probably better to control the position without the speed
+        //transform.position += new Vector3 (speed, 0f, 0f);
+        x += direc * xinc;
+
+        if (x > 5f || x < -5f) {
+            direc = -direc;
+        }
+        pos.x = x;
+        transform.position = new Vector3 (x, 0f, 0f);
+
     }
 
-    void fancy () {
-        a = 5 + b;
-    }
 }
